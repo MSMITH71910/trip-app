2ae4354484d853c585b22c90baccc20229d2a5b2
MSMITH71910 <m.smith71910@gmail.com>
2026-05-07 18:13:00 -0400

    Fix STORAGES config and include static directory

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -107,14 +107,22 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Simpler WhiteNoise storage for initial deployment success
-STATICFILES_STORAGE = 'whitenoise.storage.WhiteNoiseStorage'
+# Modern Django 4.2+ Storage Configuration
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.WhiteNoiseStorage",
+    },
+}
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
