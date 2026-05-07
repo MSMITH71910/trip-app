83698564136b8cfe390d0f42cd736f95f221179d
MSMITH71910 <m.smith71910@gmail.com>
2026-05-07 18:00:39 -0400

    Add build script for Vercel migrations

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building the project..."
+python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
+
+echo "Running migrations..."
+python3 manage.py migrate --noinput
+
+echo "Build complete."
