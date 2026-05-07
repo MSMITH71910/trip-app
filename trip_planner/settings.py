ef5982f62525594e3df29e09c573d400e6428cb5
MSMITH71910 <m.smith71910@gmail.com>
2026-05-07 17:44:24 -0400

    Fix bootstrap5 app name in settings

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_cleanup.apps.CleanupConfig',
-    'bootstrap5',
+    'django_bootstrap5',
     'users',
     'trips',
     'itineraries',
